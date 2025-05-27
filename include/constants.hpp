3aab3b8bfd999471fb3c672e84cd56f0323424ec
Jiayi Wang <jjiayu889@gmail.com>
2025-05-27 13:19:27 +0800

    add long stairs terrain

--- include/constants.hpp
+++ include/constants.hpp
@@ -23,13 +23,13 @@ const int stance_foot_at_goal = LEFT_FOOT;
 const Vector_3 goal_offset(0.0, 0.0, 0.0); // goal offset applied to move in the local frame of the goal surface (last surface)
 
 // Define Number of Steps
-const int total_num_steps = 100;
+const int total_num_steps = 15;
 
 // Define Current (Initial) Foot Position
 const int current_stance_foot_flag = RIGHT_FOOT;
-const Point_3 current_foot_pos(2.2, 0.7, 0.0); // starting point for two flat
+// const Point_3 current_foot_pos(2.2, 0.7, 0.0); // starting point for two flat
 // const Point_3 current_foot_pos(0.1, 0.0, 0.0); // starting point for stairs
-// const Point_3 current_foot_pos(0.0, 0.0, 0.0); // starting point for long stairs
+const Point_3 current_foot_pos(0.0, 0.0, 0.0); // starting point for long stairs
 
 // Node similarity threshold (compare for centroid distance and/or perimeter distance)
 const double node_similarity_threshold = 0.02;
@@ -87,183 +87,184 @@ const std::vector<std::vector<Point_3>> surf_list = {
     //----------------------
     
     //----------------------
-    // Two flat surfaces
-    // Surface 1
-    {
-        Point_3(0.0, 0.0, 0.0),  // bottom left
-        Point_3(5.45, 0.0, 0.0),  // bottom right
-        Point_3(5.45, 1.0, 0.0),  // top right
-        Point_3(0.0, 1.0, 0.0)   // top left
-    },
-    // Surface 2: Another rectangle or surface
-    {
-        Point_3(5.5,  0.0, 0.0),  // bottom left
-        Point_3(7.0, 0.0, 0.0),  // bottom right
-        Point_3(7.0, 1.0, 0.0),  // top right
-        Point_3(5.5,  1.0, 0.0)   // top left
-    },
+    // // Two flat surfaces
+    // // Surface 1
+    // {
+    //     Point_3(0.0, 0.0, 0.0),  // bottom left
+    //     Point_3(5.45, 0.0, 0.0),  // bottom right
+    //     Point_3(5.45, 1.0, 0.0),  // top right
+    //     Point_3(0.0, 1.0, 0.0)   // top left
+    // },
+    // // Surface 2: Another rectangle or surface
+    // {
+    //     Point_3(5.5,  0.0, 0.0),  // bottom left
+    //     Point_3(7.0, 0.0, 0.0),  // bottom right
+    //     Point_3(7.0, 1.0, 0.0),  // top right
+    //     Point_3(5.5,  1.0, 0.0)   // top left
+    // },
     //----------------------
 
     // // Add more surfaces as needed
     //----------------------
-    // // Long Stairs
-    // // Floor
-    // {
-    //     Point_3(0.12, -1.0, 0.0),   // bottom left
-    //     Point_3(-0.5, -1.0, 0.0),   // bottom right
-    //     Point_3(-0.5, 1.0, 0.0),    // top right
-    //     Point_3(0.12, 1.0, 0.0)     // top left
-    // },
+    // Long Stairs
+    // Floor
+    {
+        Point_3(0.12, 1.0, 0.0),    // bottom left
+        Point_3(-0.5, 1.0, 0.0),    // bottom right
+        Point_3(-0.5, -1.0, 0.0),   // top right
+        Point_3(0.12, -1.0, 0.0)    // top left
+    },
+    
+    // 1st stair
+    {
+        Point_3(0.42, 1.0, 0.1),    // bottom left
+        Point_3(0.12, 1.0, 0.1),    // bottom right
+        Point_3(0.12, 0.5, 0.1),    // top right
+        Point_3(0.42, 0.5, 0.1)     // top left
+    },
+    {
+        Point_3(0.42, -0.5, 0.1),   // bottom left
+        Point_3(0.12, -0.5, 0.1),   // bottom right
+        Point_3(0.12, -1.0, 0.1),   // top right
+        Point_3(0.42, -1.0, 0.1)    // top left
+    },
+    // 2nd stair
+    {
+        Point_3(0.725, 0.8, 0.2),   // bottom left
+        Point_3(0.425, 0.8, 0.2),   // bottom right
+        Point_3(0.425, 0, 0.2),     // top right
+        Point_3(0.725, 0, 0.2)      // top left
+    },
+    {
+        Point_3(0.725, -0.5, 0.2),  // bottom left
+        Point_3(0.425, -0.5, 0.2),  // bottom right
+        Point_3(0.425, -1.0, 0.2),  // top right
+        Point_3(0.725, -1.0, 0.2)   // top left
+    },
 
-    // // 1st stair
-    // {
-    //     Point_3(0.12, 0.5, 0.1),    // bottom left
-    //     Point_3(0.42, 0.5, 0.1),    // bottom right
-    //     Point_3(0.42, 1.0, 0.1),    // top right
-    //     Point_3(0.12, 1.0, 0.1)     // top left
-    // },
-    // {
-    //     Point_3(0.12, -1.0, 0.1),   // bottom left
-    //     Point_3(0.42, -1.0, 0.1),   // bottom right
-    //     Point_3(0.42, -0.5, 0.1),   // top right
-    //     Point_3(0.12, -0.5, 0.1)    // top left
-    // },
+    // 3rd stair
+    {
+        Point_3(1.03, 1.0, 0.3),    // bottom left
+        Point_3(0.73, 1.0, 0.3),    // bottom right
+        Point_3(0.73, -1.0, 0.3),   // top right
+        Point_3(1.03, -1.0, 0.3)    // top left
+    },
 
-    // // 2nd stair
-    // {
-    //     Point_3(0.425, 0.0, 0.2),   // bottom left
-    //     Point_3(0.725, 0.0, 0.2),   // bottom right
-    //     Point_3(0.725, 0.8, 0.2),   // top right
-    //     Point_3(0.425, 0.8, 0.2)    // top left
-    // },
-    // {
-    //     Point_3(0.425, -1.0, 0.2),  // bottom left
-    //     Point_3(0.725, -1.0, 0.2),  // bottom right
-    //     Point_3(0.725, -0.5, 0.2),  // top right
-    //     Point_3(0.425, -0.5, 0.2)   // top left
-    // },
+    // 4th stair
+    {
+        Point_3(1.33, 1.0, 0.4),    // bottom left
+        Point_3(1.03, 1.0, 0.4),    // bottom right
+        Point_3(1.03, 0, 0.4),      // top right
+        Point_3(1.33, 0, 0.4)       // top left
+    },
+    {
+        Point_3(1.33, -0.5, 0.4),   // bottom left
+        Point_3(1.03, -0.5, 0.4),   // bottom right
+        Point_3(1.03, -1.0, 0.4),   // top right
+        Point_3(1.33, -1.0, 0.4)    // top left
+    },
 
-    // // 3rd stair
-    // {
-    //     Point_3(0.73, -1.0, 0.3),   // bottom left
-    //     Point_3(1.03, -1.0, 0.3),   // bottom right
-    //     Point_3(1.03, 0.5, 0.3),    // top right
-    //     Point_3(0.73, 0.5, 0.3)     // top left
-    // },
+    // 5th stair
+    {
+        Point_3(1.63, 1.0, 0.5),    // bottom left
+        Point_3(1.33, 1.0, 0.5),    // bottom right
+        Point_3(1.33, 0.5, 0.5),    // top right
+        Point_3(1.63, 0.5, 0.5)     // top left
+    },
+    {
+        Point_3(1.63, -0.5, 0.5),   // bottom left
+        Point_3(1.33, -0.5, 0.5),   // bottom right
+        Point_3(1.33, -1.0, 0.5),   // top right
+        Point_3(1.63, -1.0, 0.5)    // top left
+    },
 
-    // // 4th stair
-    // {
-    //     Point_3(1.03, 0.0, 0.4),    // bottom left
-    //     Point_3(1.33, 0.0, 0.4),    // bottom right
-    //     Point_3(1.33, 1.0, 0.4),    // top right
-    //     Point_3(1.03, 1.0, 0.4)     // top left
-    // },
-    // {
-    //     Point_3(1.03, -1.0, 0.4),   // bottom left
-    //     Point_3(1.33, -1.0, 0.4),   // bottom right
-    //     Point_3(1.33, -0.5, 0.4),   // top right
-    //     Point_3(1.03, -0.5, 0.4)    // top left
-    // },
+    // 6th stair
+    {
+        Point_3(1.93, 1.0, 0.6),    // bottom left
+        Point_3(1.63, 1.0, 0.6),    // bottom right
+        Point_3(1.63, -1.0, 0.6),   // top right
+        Point_3(1.93, -1.0, 0.6)    // top left
+    },
 
-    // // 5th stair
-    // {
-    //     Point_3(1.33, 0.5, 0.5),    // bottom left
-    //     Point_3(1.63, 0.5, 0.5),    // bottom right
-    //     Point_3(1.63, 1.0, 0.5),    // top right
-    //     Point_3(1.33, 1.0, 0.5)     // top left
-    // },
-    // {
-    //     Point_3(1.33, -1.0, 0.5),   // bottom left
-    //     Point_3(1.63, -1.0, 0.5),   // bottom right
-    //     Point_3(1.63, -0.5, 0.5),   // top right
-    //     Point_3(1.33, -0.5, 0.5)    // top left
-    // },
+    // 7th stair
+    {
+        Point_3(2.23, 0.75, 0.7),   // bottom left
+        Point_3(1.93, 0.75, 0.7),   // bottom right
+        Point_3(1.93, 0.25, 0.7),   // top right
+        Point_3(2.23, 0.25, 0.7)    // top left
+    },
+    {
+        Point_3(2.23, -0.5, 0.7),   // bottom left
+        Point_3(1.93, -0.5, 0.7),   // bottom right
+        Point_3(1.93, -1.0, 0.7),   // top right
+        Point_3(2.23, -1.0, 0.7)    // top left
+    },
 
-    // // 6th stair
-    // {
-    //     Point_3(1.63, -1.0, 0.6),   // bottom left
-    //     Point_3(1.93, -1.0, 0.6),   // bottom right
-    //     Point_3(1.93, 1.0, 0.6),    // top right
-    //     Point_3(1.63, 1.0, 0.6)     // top left
-    // },
+    // 8th stair
+    {
+        Point_3(2.53, 0.75, 0.8),   // bottom left
+        Point_3(2.23, 0.75, 0.8),   // bottom right
+        Point_3(2.23, -0.7, 0.8),   // top right
+        Point_3(2.53, -0.7, 0.8)    // top left
+    },
+    {
+        Point_3(2.53, -0.5, 0.8),   // bottom left
+        Point_3(2.23, -0.5, 0.8),   // bottom right
+        Point_3(2.23, -1.0, 0.8),   // top right
+        Point_3(2.53, -1.0, 0.8)    // top left
+    },
 
-    // // 7th stair
-    // {
-    //     Point_3(1.93, 0.25, 0.7),   // bottom left
-    //     Point_3(2.23, 0.25, 0.7),   // bottom right
-    //     Point_3(2.23, 0.75, 0.7),   // top right
-    //     Point_3(1.93, 0.75, 0.7)    // top left
-    // },
-    // {
-    //     Point_3(1.93, -1.0, 0.7),   // bottom left
-    //     Point_3(2.23, -1.0, 0.7),   // bottom right
-    //     Point_3(2.23, -0.5, 0.7),   // top right
-    //     Point_3(1.93, -0.5, 0.7)    // top left
-    // },
+    // 9th stair (skip breaking)
+    {
+        Point_3(2.83, 0.75, 0.9),   // bottom left
+        Point_3(2.53, 0.75, 0.9),   // bottom right
+        Point_3(2.53, 0.25, 0.9),   // top right
+        Point_3(2.83, 0.25, 0.9)    // top left
+    },
 
-    // // 8th stair
-    // {
-    //     Point_3(2.23, -0.7, 0.8),   // bottom left
-    //     Point_3(2.53, -0.7, 0.8),   // bottom right
-    //     Point_3(2.53, 0.75, 0.8),   // top right
-    //     Point_3(2.23, 0.75, 0.8)    // top left
-    // },
-    // {
-    //     Point_3(2.23, -1.0, 0.8),   // bottom left
-    //     Point_3(2.53, -1.0, 0.8),   // bottom right
-    //     Point_3(2.53, -0.5, 0.8),   // top right
-    //     Point_3(2.23, -0.5, 0.8)    // top left
-    // },
+    {
+        Point_3(2.83, -0.5, 0.9),   // bottom left
+        Point_3(2.53, -0.5, 0.9),   // bottom right
+        Point_3(2.53, -1.0, 0.9),   // top right
+        Point_3(2.83, -1.0, 0.9)    // top left
+    },
 
-    // // 9th stair
-    // {
-    //     Point_3(2.53, 0.25, 0.9),   // bottom left
-    //     Point_3(2.83, 0.25, 0.9),   // bottom right
-    //     Point_3(2.83, 0.75, 0.9),   // top right
-    //     Point_3(2.53, 0.75, 0.9)    // top left
-    // },
-    // {
-    //     Point_3(2.53, -1.0, 0.9),   // bottom left
-    //     Point_3(2.83, -1.0, 0.9),   // bottom right
-    //     Point_3(2.83, -0.5, 0.9),   // top right
-    //     Point_3(2.53, -0.5, 0.9)    // top left
-    // },
 
-    // // 10th stair
-    // {
-    //     Point_3(2.83, 0.0, 1.0),    // bottom left
-    //     Point_3(3.13, 0.0, 1.0),    // bottom right
-    //     Point_3(3.13, 1.0, 1.0),    // top right
-    //     Point_3(2.83, 1.0, 1.0)     // top left
-    // },
-    // {
-    //     Point_3(2.83, -1.0, 1.0),   // bottom left
-    //     Point_3(3.13, -1.0, 1.0),   // bottom right
-    //     Point_3(3.13, -0.5, 1.0),   // top right
-    //     Point_3(2.83, -0.5, 1.0)    // top left
-    // },
+    // 10th stair
+    {
+        Point_3(3.13, 1.0, 1.0),    // bottom left
+        Point_3(2.83, 1.0, 1.0),    // bottom right
+        Point_3(2.83, 0.0, 1.0),    // top right
+        Point_3(3.13, 0.0, 1.0)     // top left
+    },
+    {
+        Point_3(3.13, -0.5, 1.0),   // bottom left
+        Point_3(2.83, -0.5, 1.0),   // bottom right
+        Point_3(2.83, -1.0, 1.0),   // top right
+        Point_3(3.13, -1.0, 1.0)    // top left
+    },
 
-    // // 11th stair
-    // {
-    //     Point_3(3.13, 0.0, 1.1),    // bottom left
-    //     Point_3(3.43, 0.0, 1.1),    // bottom right
-    //     Point_3(3.43, 0.5, 1.1),    // top right
-    //     Point_3(3.13, 0.5, 1.1)     // top left
-    // },
-    // {
-    //     Point_3(3.13, -1.0, 1.1),   // bottom left
-    //     Point_3(3.43, -1.0, 1.1),   // bottom right
-    //     Point_3(3.43, -0.5, 1.1),   // top right
-    //     Point_3(3.13, -0.5, 1.1)    // top left
-    // },
+    // 11th stair
+    {
+        Point_3(3.43, 0.5, 1.1),    // bottom left
+        Point_3(3.13, 0.5, 1.1),    // bottom right
+        Point_3(3.13, 0.0, 1.1),    // top right
+        Point_3(3.43, 0.0, 1.1)     // top left
+    },
+    {
+        Point_3(3.43, -0.5, 1.1),   // bottom left
+        Point_3(3.13, -0.5, 1.1),   // bottom right
+        Point_3(3.13, -1.0, 1.1),   // top right
+        Point_3(3.43, -1.0, 1.1)    // top left
+    },
 
-    // // 12th stair (target floor)
-    // {
-    //     Point_3(3.43, -1.0, 1.2),   // bottom left
-    //     Point_3(3.73, -1.0, 1.2),   // bottom right
-    //     Point_3(3.73, 1.0, 1.2),    // top right
-    //     Point_3(3.43, 1.0, 1.2)     // top left
-    // }
+    // Target floor (12th stair)
+    {
+        Point_3(3.73, 1.0, 1.2),    // bottom left
+        Point_3(3.43, 1.0, 1.2),    // bottom right
+        Point_3(3.43, -1.0, 1.2),   // top right
+        Point_3(3.73, -1.0, 1.2)    // top left
+    }
 };
 
 }
