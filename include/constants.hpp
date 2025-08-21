a9f96a452b61f8f0d9f3be6b11c62de1c410df3c
Jiayi Wang <jjiayu889@gmail.com>
2025-08-21 15:04:27 +0800

    bring cycle complete for a star

--- include/constants.hpp
+++ include/constants.hpp
@@ -13,8 +13,8 @@ const int LEFT_FOOT = 0;   // Represents the left foot in stance
 const int RIGHT_FOOT = 1;  // Represents the right foot in stance
 
 // Foot size information
-const double foot_length = 0.0;//0.22;
-const double foot_width  = 0.0;//0.12;
+const double foot_length = 0.22;//0.22;
+const double foot_width  = 0.12;//0.12;
 
 // Merge Node Flag
 const bool merge_node_flag = true;
@@ -61,7 +61,7 @@ const std::string node_search_method = "bruteforce"; // "bruteforce", "kdtree", 
 // const std::vector<std::vector<Point_3>> surf_list = TwoFlatSurfaces;
 // const std::vector<std::vector<Point_3>> surf_list = LongStairsComplete;
 // const std::vector<std::vector<Point_3>> surf_list = LongStairs;
-// const std::vector<std::vector<Point_3>> surf_list = LongLongStairs;
-const std::vector<std::vector<Point_3>> surf_list = ThreePathsScene;
+const std::vector<std::vector<Point_3>> surf_list = LongLongStairs;
+// const std::vector<std::vector<Point_3>> surf_list = ThreePathsScene;
 
 }
